6359dae66f139fd81651870c3ec75743e92cca44
wuqing.zhu <16621247894zwq@sina.com>
2021-09-16 18:17:11 +0800

    修改mysql版本，新增前端使用教程

--- VueAdminJava/src/main/java/com/zwq/cloud/entity/BaseEntity.java
+++ VueAdminJava/src/main/java/com/zwq/cloud/entity/BaseEntity.java
@@ -4,10 +4,11 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
-public class BaseEntity {
+public class BaseEntity implements Serializable {
 
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
